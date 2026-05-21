93e9045279faa13bc8ada50cf6b48531550c6b5f
Xiêng Lăng Đức Mạnh <manh105963@donga.edu.vn>
2026-05-21 17:25:49 +0700

    update student manager

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,6 @@
-﻿INSERT INTO students (name, email) VALUES
+INSERT INTO students (name, email) VALUES
 ('Nguyen Van A', 'a@gmail.com'),
 ('Tran Thi B', 'b@gmail.com'),
 ('Le Van C', 'c@gmail.com'),
 ('Pham Quoc Khang', 'khang@gmail.com'),
-('Nguyen Minh Huy', 'huy@gmail.com'),
-('Tran Ngoc Mai', 'mai@gmail.com');
+('Nguyen Minh Huy', 'huy@gmail.com');
